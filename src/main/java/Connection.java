48c493308fd2c4d80b0bfb61f76ea2d5f2bf9652
Andreev Alexey <andreev-j@yandex.ru>
2021-12-28 14:03:07 +0700

    Add query params

--- src/main/java/Connection.java
+++ src/main/java/Connection.java
@@ -1,15 +1,18 @@
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.*;
+
 
 public class Connection implements Runnable{
     private final Socket socket;
     private BufferedOutputStream out;
+    private static final int LIMIT = 4096;
+
+    private Map<String, List<String>> queryParams;
 
     // конструктор
     public Connection(Socket socket) {
@@ -61,6 +64,63 @@ public class Connection implements Runnable{
         }
     }
 
+    private String decode(String value) {
+        String result = "";
+        try {
+            result = URLDecoder.decode(value, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private Map<String, List<String>> getQueryParams(String query) {
+        if(query == null) return null;
+        String name, value;
+        Map<String, List<String>> result = new HashMap<>();
+        // массив пар параметр=значение
+        String[] params = query.split("&");
+        // цикл по парам
+        for(String param : params) {
+            List<String> values = new ArrayList<>();
+            // разбиваем на параметр и значение
+            name = param.split("=")[0];
+            value = decode(param.split("=")[1]);
+            // параметр уже присутствует в мапе
+            if(result.containsKey(name)) {
+                // список значений параметра из мапы
+                values = result.get(name);
+                // добавляем новое значение
+                values.add(value);
+                // обновляем в мапе
+                result.replace(name, values);
+            } else {
+                // добавляем значение в список
+                values.add(value);
+                // добавляем в мапу
+                result.put(name, values);
+            }
+        }
+        return result;
+    }
+
+    private String getParamValue(String name) {
+        if(!queryParams.containsKey(name)) return null;
+        StringBuilder result = new StringBuilder();
+        List<String> values = queryParams.get(name);
+        switch (values.size()) {
+            case 0:
+                break;
+            case 1:
+                result.append(values.get(0));
+                break;
+            default:
+                for (String value : values) {
+                    result.append(value).append("\n");
+                }
+        }
+        return result.toString();
+    }
     // обработка запроса
     @Override
     public void run() {
@@ -82,7 +142,14 @@ public class Connection implements Runnable{
                 return;
             }
             // путь к файлу
-            final String path = parts[1];
+            String path = "";
+            try {
+                URI uri = new URI(parts[1]);
+                path = uri.getPath();
+                queryParams = getQueryParams(uri.getRawQuery());
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
             // ссылка на путь из списка доступных сервера
             final ValidPaths validPaths = ValidPaths.getValueByPath(path);
             // проверка на наличие в списке доступных
@@ -116,4 +183,5 @@ public class Connection implements Runnable{
             }
         }
     }
+
 }
